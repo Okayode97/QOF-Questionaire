38c4924b0faeb9e3ed73d6a5a1fd65b64e60b186
Kayode Sonaike <Okayode97@gmail.com>
2020-12-20 13:38:18 +0000

    Changes made - Added docstring for functions in QuestionnaireServices file - Added  new function createQuestionnaireObject, to the questionnaireService   class - declared asset in flutter pubsec.yaml file

--- lib/questionnaire_from_json.dart
+++ lib/questionnaire_from_json.dart
@@ -1,15 +1,25 @@
+//https://flutter.dev/docs/development/ui/assets-and-images
+import 'package:flutter/services.dart' show rootBundle;
+import 'dart:convert';
 
 // https://www.tutorialspoint.com/dart_programming/dart_programming_enumeration.htm
 enum ChronicConditions {
   // define a comma-seperated list of identifiers represented by an integar value, zero indexed.
-  // as an initial demo it will expand to include 2 different chronic conditions
   htn,
 }
 
 class QuestionnaireServices {
 
-  // get the path of the json file for each chronic condition
-  String _getCCQuestionnairePath(ChronicConditions conditions){
+  String _get_Chronic_Condition_QuestionnairePath(ChronicConditions conditions){
+  /*
+    function returns the path of the json file given the enumeration contained in enums/questionnaire_type.dart
+     
+    Args:
+      questionnaireType (QuestionnaireType): An instance of the enum class
+    
+    Returns:
+      (String): PATH to the json file containing the string
+  */
     switch(conditions) {
       case ChronicConditions.htn:
         return 'chronic_conditions/htn.json';
@@ -18,6 +28,20 @@ class QuestionnaireServices {
     }
   }
 
-  // Return a questionnaire object
+
+  Future<bool> createQuestionnaireObject(ChronicConditions conditions) async{
+  /*
+    function returns a future of the Questionnaire class defined in models/questionnaire.dart.
+
+    Args:
+      conditions (ChronicConditions): An instance of the enum class
+    
+    Returns:
+      a questionnaire object
+  */
+    final jsonPath = _get_Chronic_Condition_QuestionnairePath(conditions);
+    final jsonData = await rootBundle.loadString(jsonPath);
+    final jsonDataDecoded = jsonDecode(jsonData);
+  }
 
 }
