5a6ce5db56888d96d27dca8aa71e79ebb3878790
Kayode Sonaike <Okayode97@gmail.com>
2021-01-01 17:28:41 +0000

    Filled our most of the json file for the chronic condition, updated the enum class and the paths to the json file in the chronic condition services.dart

--- lib/Services/chroniccondition_services.dart
+++ lib/Services/chroniccondition_services.dart
@@ -6,6 +6,21 @@ import 'package:qof_sample_framework/Models/questionnaire.dart';
 enum ChronicConditions {
   // define a comma-seperated list of identifiers represented by an integar value, zero indexed.
   htn,
+  af,
+  cancer,
+  cardiovascular,
+  chronic_kidney_disease,
+  contraception,
+  copd,
+  depression,
+  diabetes_meltitus,
+  epilepsy,
+  hypothydroism,
+  immunisation,
+  mental_health_disorder,
+  osteoporosis,
+  ra,
+  tia
 }
 
 class QuestionnaireServices {
@@ -23,6 +38,36 @@ class QuestionnaireServices {
     switch(conditions) {
       case ChronicConditions.htn:
         return 'chronic_conditions/htn.json';
+      case ChronicConditions.af:
+        return 'chronic_conditions/Af.json';
+      case ChronicConditions.cancer:
+        return 'chronic_conditions/Cancer.json';
+      case ChronicConditions.cardiovascular:
+        return 'chronic_conditions/Cardiovascular.json';
+      case ChronicConditions.chronic_kidney_disease:
+        return 'chronic_conditions/Chronic_kidney_disease.json';
+      case ChronicConditions.contraception:
+        return 'chronic_conditions/Contraception.json';
+      case ChronicConditions.copd:
+        return 'chronic_conditions/COPD.json';
+      case ChronicConditions.depression:
+        return 'chronic_conditions/Depression.json';
+      case ChronicConditions.diabetes_meltitus:
+        return 'chronic_conditions/Diabetes_Mellitus.json';
+      case ChronicConditions.epilepsy:
+        return 'chronic_conditions/Epilepsy.json';
+      case ChronicConditions.hypothydroism:
+        return 'chronic_conditions/Hypothydroism.json';
+      case ChronicConditions.immunisation:
+        return 'chronic_conditions/Immunisation.json';
+      case ChronicConditions.mental_health_disorder:
+        return 'chronic_conditions/Mental_health_disorder.json';
+      case ChronicConditions.osteoporosis:
+        return 'chronic_conditions/Osteoporosis.json';
+      case ChronicConditions.ra:
+        return 'chronic_conditions/RA.json';
+      case ChronicConditions.tia:
+        return 'chronic_conditions/TIA.json';
       default:
         return null;
     }
